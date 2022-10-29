45af885fc96b8ef9eb552b92f5c3d857b6019934
Zion Aitken <zionaitken@gmail.com>
2022-10-29 10:01:54 +0100

    We got da project
    Unreal project has been added.

---
+++ Source/SpookyJam/SpookyJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpookyJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPOOKYJAM_API ASpookyJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
